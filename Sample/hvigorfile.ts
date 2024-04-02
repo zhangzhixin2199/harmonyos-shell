cfc706de0561f22203051146a5f5e0cd730df370
zhangzhixin <zhangzhixin@58.com>
2024-04-02 16:51:25 +0800

    添加小程序配置插件逻辑

--- Sample/hvigorfile.ts
+++ Sample/hvigorfile.ts
@@ -1,2 +1,64 @@
-// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
-export { hapTasks } from '@ohos/hvigor-ohos-plugin';
+import { hvigor, HvigorNode, HvigorPlugin,parseJsonFile,Json5Reader} from '@ohos/hvigor';
+import { hapTasks, OhosHapContext, OhosPluginId, Target} from '@ohos/hvigor-ohos-plugin';
+import { transformSync } from '@babel/core';
+import * as fs from 'fs'
+
+export function customPlugin(): HvigorPlugin {
+  return {
+    pluginId: 'customPlugin',
+    context() {
+      return {
+        data: 'customPlugin xxx'
+      };
+    },
+    async apply(currentNode: HvigorNode): Promise<void> {
+      hvigor.nodesEvaluated(async () => {
+        // 注册模块级任务
+        hapTask(currentNode);
+      });
+    }
+  };
+}
+function hapTask(currentNode: HvigorNode) {
+    console.log('hapTask:'+currentNode.toString());
+    const hapContext = currentNode.getContext(OhosPluginId.OHOS_HAP_PLUGIN) as OhosHapContext;
+    const nodepath = hapContext?.getModulePath()
+    hapContext?.targets((target: Target) => {
+      const targetName = target?.getTargetName()
+      const outputPath = target.getBuildTargetOutputPath();
+      currentNode.registerTask({
+        name: `${targetName}@CompileMiniConfig`,
+        run(taskContext: HvigorTaskContext) {
+          const path = require('path');
+          let appFilePath = path.join(nodepath, "mini-config.json5")
+          if (!fs.existsSync(appFilePath)) {
+            console.warn('mini-config.json5 does not exist,Please set the configuration file');
+            return
+          }
+          let json=''
+          try{
+            json = Json5Reader.getJson5Obj(appFilePath,'utf8')
+          }catch(error){
+            console.error('mini-config.json5 is incorrectly formatted')
+            console.error(error);
+          }
+          console.log(json);
+          if (json =='') {
+            console.warn('mini-config.json5 this file are empty,Please set the configuration');
+            return
+          }
+          const jsonContent = JSON.stringify(json)
+          let replacePath = outputPath.replace('default/outputs', 'default/intermediates/res');
+          let outputFilePath = path.join(replacePath, "resources", "rawfile", "mini-config.json")
+          fs.writeFileSync(outputFilePath, jsonContent)
+        },
+        dependencies: [`${targetName}@CompileResource`],
+        postDependencies: ['assembleHap']
+      });
+    });
+}
+
+export default {
+  system: hapTasks,
+  plugins:[customPlugin()]
+};
